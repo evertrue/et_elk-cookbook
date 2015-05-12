fe4a33ff112daa82c55f8a5087bb9c0dcab95735
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-12 17:24:14 -0400

    Start to pull all the cookbooks together

---
+++ recipes/client.rb
@@ -0,0 +1,4 @@
+
+include_recipe 'elk_forwarder::certs'
+
+include_recipe 'elk_forwarder::default'
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,3 +15,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+include_recipe 'elk_elasticsearch'
+
+include_recipe 'elk_logstash::certs'
+
+include_recipe 'elk_logstash'
+
+include_recipe 'elk_kibana'
