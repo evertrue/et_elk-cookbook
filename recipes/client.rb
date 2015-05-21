8d79c565b556ffe175a5c2892355bcfb95be3bb2
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-21 16:16:10 -0400

    Add a test syslog filter (this is a regression)

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,4 +1,20 @@
+servers = search(
+  :node,
+  node['elk']['logstash_search_str'] +
+  " AND chef_environment:#{node.chef_environment}"
+).map do |node|
+  "#{node['fqdn']}:5043"
+end
+
+node.set['elk_forwarder']['config']['network']['servers'] = servers
 
 include_recipe 'elk_forwarder::certs'
+
+elk_forwarder_log 'syslog' do
+  paths [
+    '/var/log/syslog'
+  ]
+  fields type: :syslog
+end
 
 include_recipe 'elk_forwarder::default'
