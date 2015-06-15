ebd20c924074cbdf66f553deae52eeb8d02d8a4a
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-15 16:27:15 -0400

    Set Templates Cookbook to elk

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,9 +1,11 @@
 logstash_pattern 'rails' do
+  templates_cookbook 'elk'
   instance 'server'
   templates 'pattern_rails_app' => 'pattern_rails_app.erb'
 end
 
 logstash_config 'rails app filter' do
+  templates_cookbook 'elk'
   templates 'filter_rails_app' => 'filter_rails_app.erb'
   instance 'server'
   variables node['elk_logstash']['server']
