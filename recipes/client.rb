65445875edb8e7517b3ecbb096b832de597517c9
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-17 13:50:10 -0400

    Add Fixup to wipe v1.0 files attribute

--- recipes/client.rb
+++ recipes/client.rb
@@ -6,6 +6,12 @@ servers = search(
   "#{node['fqdn']}:5043"
 end
 
+# Wipe legacy format of files
+if node['elk_forwarder']['config']['files'].is_a?(Array)
+  node.rm('elk_forwarder', 'config', 'files')
+  return
+end
+
 node.set['elk_forwarder']['config']['network']['servers'] = servers
 
 include_recipe 'elk_forwarder::certs'
