d56e759b3659cc7947687df5ae3d6706837dcc6c
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-17 13:18:01 -0400

    Remove shuffling logic as it is not necessary b/c LF will randomly select a server

--- recipes/client.rb
+++ recipes/client.rb
@@ -6,11 +6,7 @@ servers = search(
   "#{node['fqdn']}:5043"
 end
 
-# We set the list of servers if they have changed
-if node['elk_forwarder']['config']['network']['servers'].sort != servers.sort
-  # Shuffle up the list so that we have a semi even distribution of load
-  node.set['elk_forwarder']['config']['network']['servers'] = servers.shuffle
-end
+node.set['elk_forwarder']['config']['network']['servers'] = servers
 
 include_recipe 'elk_forwarder::certs'
 
