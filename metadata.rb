225e5cdf18a7cb526b13e37dfe3c3eed50af8d17
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-17 13:27:31 -0400

    Bump to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eddie.hurtig@evertrue.com'
 license          'apache2'
 description      'Configures an Elk Cluster'
 long_description 'Installs/Configures elk'
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'ubuntu', '>= 12.04'
 
