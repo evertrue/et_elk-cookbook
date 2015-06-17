855797ceb5bbf42da80a68a70e5410cfd8f42690
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-17 13:50:10 -0400

    Add Fixup to wipe v1.0 files attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,9 @@
 default['elk']['logstash_search_str'] = 'recipes:elk_logstash\:\:default'
 
+# Wipe legacy format of files
+if node['elk_forwarder']['config']['files'].is_a?(Array)
+  node.rm('elk_forwarder', 'config', 'files')
+end
+
 set['elk_forwarder']['config']['files']['syslog']['paths'] = ['/var/log/syslog']
 set['elk_forwarder']['config']['files']['syslog']['fields']['type'] = 'syslog'
