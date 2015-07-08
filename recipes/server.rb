18ae211278c6eab5d21706f7d3f57c3244ab5df0
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-07-08 17:10:19 -0400

    Include the java filter in the LS Configuration

--- recipes/server.rb
+++ recipes/server.rb
@@ -42,6 +42,13 @@ logstash_config 'rails app filter' do
   variables node['elk']['server']
 end
 
+logstash_config 'java filter' do
+  templates_cookbook 'elk'
+  templates 'filter_java' => 'filter_java.erb'
+  instance 'server'
+  variables node['elk']['server']
+end
+
 logstash_config 'haproxy http filter' do
   templates_cookbook 'elk'
   templates 'filter_haproxy_http' => 'filter_haproxy_http.erb'
