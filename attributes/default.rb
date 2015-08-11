a92b24f89adf70c12492264955a634b27aaa4642
Eric Herot <eric.github@herot.com>
2015-08-11 12:11:31 -0400

    Move attributes to their appropriate files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,1 @@
-default['et_elk']['logstash_search_str'] = 'recipes:elk_logstash\:\:default'
-
-# Wipe legacy format of files
-if node['elk_forwarder']['config']['files'].is_a?(Array)
-  node.rm('elk_forwarder', 'config', 'files')
-end
-
-set['elk_forwarder']['config']['files']['syslog']['paths'] = ['/var/log/syslog']
-set['elk_forwarder']['config']['files']['syslog']['fields']['type'] = 'syslog'
-
-set['elk_forwarder']['certs']['ca_data_bag'] = 'certificates'
-set['elk_forwarder']['certs']['ca_data_bag_item'] = 'logstash'
-set['elk_forwarder']['certs']['ca_data_bag_item_key'] = 'ca certificate'
-
 set['apt']['compile_time_update'] = true
