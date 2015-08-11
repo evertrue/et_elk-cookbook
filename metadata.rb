5ad6c0813d15d2d7a7b14f03a08271076161cc81
Eric Herot <eric.github@herot.com>
2015-08-11 15:01:11 -0400

    Drop support for 12.04

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Configures an Elk Cluster'
 long_description 'Installs/Configures elk'
 version          '1.3.5'
 
-supports 'ubuntu', '>= 12.04'
+supports 'ubuntu', '>= 14.04'
 
 depends 'elk_elasticsearch', '~> 0.1'
 depends 'elk_logstash', '~> 0.0'
