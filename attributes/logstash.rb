9ddc0cbdcf4722bf26a0e0c36f6cc3d50ac416c1
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-11-07 00:24:59 -0500

    Remove Logstash Cert Config >> logserver

--- attributes/logstash.rb
+++ attributes/logstash.rb
@@ -1,13 +1,1 @@
-default['logstash']['plugins'] = []
-
-# All this junk needs to be updated
-default['elk_logstash']['certs']['certificate_data_bag'] = 'certificates'
-default['elk_logstash']['certs']['certificate_data_bag_item'] = 'logstash'
-default['elk_logstash']['certs']['certificate_data_bag_item_key'] = 'server certificate'
-default['elk_logstash']['certs']['key_data_bag'] = 'certificates'
-default['elk_logstash']['certs']['key_data_bag_item'] = 'logstash'
-default['elk_logstash']['certs']['key_data_bag_item_key'] = 'server key'
-
-default['elk']['server']['lumberjack']['ssl certificate'] =
-  '/etc/logstash/lumberjack.crt.pem'
-default['elk']['server']['lumberjack']['ssl key'] = '/etc/logstash/lumberjack.key.pem'
+default['et_elk']['logstash']['plugins'] = []
--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -35,23 +35,3 @@ end
 logstash_pattern name do
   action [:create]
 end
-
-# This needs to be updated to be cleaner, like in elk_forwarder
-%w(key certificate).each do |c|
-  ssl_thing = data_bag_item(
-    node['elk_logstash']['certs']["#{c}_data_bag"],
-    node['elk_logstash']['certs']["#{c}_data_bag_item"]
-  )[node['elk_logstash']['certs']["#{c}_data_bag_item_key"]]
-
-  directory File.dirname(node['elk']['server']['lumberjack']["ssl #{c}"]) do
-    recursive true
-  end
-
-  fail "Need SSL #{c} couldn't find it" if ssl_thing.nil? || ssl_thing.empty?
-
-  file node['elk']['server']['lumberjack']["ssl #{c}"] do
-    content ssl_thing
-    sensitive true
-    notifies :restart, 'logstash_service[server]'
-  end
-end
