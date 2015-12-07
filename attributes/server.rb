1ddc43b3b00022395115c0381ff26688056d0136
Eric Herot <eric.github@herot.com>
2015-12-07 15:20:59 -0500

    Move kibana attributes to server attributes file

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,3 +1,9 @@
+# Update/remove this
+default['elk_kibana']['cluster']['name'] = "#{node.chef_environment}-elk"
+
+set['kibana']['version'] = '4.1.1-linux-x64'
+set['kibana']['file']['url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz'
+
 default['et_elk']['server']['elasticsearch']['host'] = ''
 default['et_elk']['server']['elasticsearch']['port'] = ''
 default['et_elk']['server']['elasticsearch']['cluster'] = "#{node.chef_environment}-elk"
