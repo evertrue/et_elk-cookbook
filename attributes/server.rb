67bbda137af042acc36d00ce86686c76aa7a465f
Eric Herot <eric.github@herot.com>
2015-12-11 13:40:05 -0500

    Remove attributes that no longer do anything

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,10 +1,4 @@
-# Update/remove this
-default['elk_kibana']['cluster']['name'] = "#{node.chef_environment}-elk"
-
 set['java']['jdk_version'] = 8
 
 set['kibana']['version'] = '4.1.1-linux-x64'
 set['kibana']['file']['url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz'
-
-default['et_elk']['server']['file']['path'] = '/var/log/logstash-output.log'
-default['et_elk']['server']['file']['codec'] = 'rubydebug'
