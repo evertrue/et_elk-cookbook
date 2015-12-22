52f109feecb9393c48b357d4af542bdbee2a3b87
Eric Herot <eric.github@herot.com>
2015-12-22 13:43:38 -0500

    ServerSpec: Bump expected ES version number to 2.1.0

--- test/integration/server/serverspec/default_spec.rb
+++ test/integration/server/serverspec/default_spec.rb
@@ -23,7 +23,7 @@ describe 'et_elk::elasticsearch' do
       end
 
       it 'has the right version' do
-        expect(JSON.parse(es_api_response.body)['version']['number']).to eq('1.4.4')
+        expect(JSON.parse(es_api_response.body)['version']['number']).to eq('2.1.0')
       end
     end
   end
