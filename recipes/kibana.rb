820c2e83dd234d0e75f2cdf907a844bc6377b0a8
Eric Herot <eric.github@herot.com>
2016-01-15 17:24:09 -0500

    Modify kibana yaml template instead of installing cookbook_file

--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -1,9 +1,3 @@
+node.set['kibana']['file']['config_template_cookbook'] = cookbook_name
+
 include_recipe 'kibana_lwrp::install'
-
-service 'kibana'
-
-cookbook_file '/opt/kibana/current/config/kibana.yml' do
-  owner 'kibana'
-  group 'kibana'
-  notifies :restart, 'service[kibana]'
-end
