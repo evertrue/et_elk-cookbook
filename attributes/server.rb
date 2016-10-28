87d484ddd715fb7fdf8b761ac6504a5a156c357d
Eric Herot <eric.github@herot.com>
2016-10-28 15:59:09 -0400

    Stop pinning kibana package version

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,5 +1,1 @@
 set['java']['jdk_version'] = 8
-
-set['kibana']['version'] = '4.3.0-linux-x64'
-set['kibana']['file']['url'] =
-  'https://download.elastic.co/kibana/kibana/kibana-4.3.0-linux-x64.tar.gz'
